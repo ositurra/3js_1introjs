70cb2d1a171dba322cda448842e259554a9e4ade
Osvaldo Iturra <os.iturra@outlook.com>
2023-12-08 12:24:01 -0300

    Inicial

---
+++ index.js
@@ -0,0 +1,17 @@
+
+precio = 400000
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+
+/*Formula para valor total
+
+MULTIPLICAR .cantidad*.precio-inicial
+
+valor-total=.cantidad*.precio-inicial 
+
+*/
+
+
+
